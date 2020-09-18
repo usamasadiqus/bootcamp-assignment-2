de9e0fb05ce364e302cac80234cc1e367082f4db
Usama Sadiq <57365488+usamasadiqus@users.noreply.github.com>
2020-09-18 15:55:37 +0500

    Update userinfo.js

--- src/userinfo.js
+++ src/userinfo.js
@@ -3,7 +3,7 @@ import React from 'react';
 function UserInfo(props) {
     return (
         <div>
-            <h1>I'm enrolled in {props.userName} I'm enrolled in {props.curriculumName} and this is my {props.assignmentNo}</h1>
+            <h1>My name is {props.userName} I'm enrolled in {props.curriculumName} and this is my {props.assignmentNo}</h1>
         </div>
     );
 }
